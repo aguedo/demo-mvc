28d53ee479b5ed6f220830875024dc9432ee3052
Aguedo Valdes <aguedo@aslanta.com>
2025-05-19 09:27:36 -0500

    Add the poco classes for app settings.

---
+++ src/Aslanta.Mvc/Configs/AppInstance.cs
@@ -0,0 +1,8 @@
+namespace Aslanta.Mvc;
+
+public class AppInstance
+{
+    public string Name { get; set; } = string.Empty;
+    public string Version { get; set; } = string.Empty;
+    public string Url { get; set; } = string.Empty;
+}
---
+++ src/Aslanta.Mvc/Configs/AppSettings.cs
@@ -0,0 +1,8 @@
+namespace Aslanta.Mvc;
+
+public class AppSettings
+{
+    public string Color { get; set; } = string.Empty;
+    public AppInstance[] Apps { get; set; } = [];
+
+}
