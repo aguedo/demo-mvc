2ecf51365d504848a18db534f5644cda86cca386
Aguedo Valdes <aguedo@aslanta.com>
2025-06-10 11:43:18 -0500

    Add the demo session model.

---
+++ src/Aslanta.Mvc/Applications/DemoSessions/SessionModel.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Aslanta.Mvc.Applications.DemoSessions;
+
+public class SessionModel
+{
+    public string? Color { get; set; }
+    public string? Version { get; set; }
+}
--- src/Aslanta.Mvc/Controllers/HomeController.cs
+++ src/Aslanta.Mvc/Controllers/HomeController.cs
@@ -6,23 +6,19 @@ namespace Aslanta.Mvc.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
+    [HttpGet("")]
     public IActionResult Index()
     {
         return View();
     }
 
+    [HttpGet("privacy")]
     public IActionResult Privacy()
     {
         return View();
     }
 
+    [HttpGet("error")]
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
